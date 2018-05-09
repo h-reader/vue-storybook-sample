4468013bdd2686cf9497b4eacd62a0cbae0be586
h-reader <super.black.jack777@gmail.com>
2018-05-09 14:53:39 +0900

    色、サイズのバリエーションを追加。

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,36 @@
 import { storiesOf } from '@storybook/vue'
-import Button from '../parts/app-button.vue'
+import AppButton from '../parts/app-button.vue'
 
-storiesOf('Button', module)
+storiesOf('app-button', module)
   .add('default', () => ({
-    components: { Button },
-    template: `<Button text="default" />`
-  }));
+    components: { AppButton },
+    template: `<app-button text="default" />`
+  }))
+  .add('color', () => ({
+    components: { AppButton },
+    template: `
+      <div style="display: flex;">
+        <app-button text='primary' kind='primary'/>
+        <app-button text='default' kind='default'/>
+        <app-button text='success' kind='success'/>
+        <app-button text='info' kind='info'/>
+        <app-button text='warning' kind='warning'/>
+        <app-button text='danger' kind='danger'/>
+        <app-button text='dark' kind='dark'/>
+      </div>
+    `
+  }))
+  .add('size', () => ({
+    components: { AppButton },
+    template: `
+      <div>
+        <div style="display: flex;">
+          <app-button text='minimum' kind='primary' size="minimum"/>
+          <app-button text='small' kind='primary' size="small"/>
+          <app-button text='normal' kind='primary' size="normal"/>
+          <app-button text='large' kind='primary' size="large"/>
+        </div>
+        <div style="display:flex;"><app-button text='full' kind='primary' size="full"/></div>
+      </div>
+      `
+  }))
